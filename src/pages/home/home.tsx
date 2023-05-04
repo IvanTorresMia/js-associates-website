4cfa17d4e572f415b4f17cb17fc8d2d0417a6490
IvanTorresMia <ivantorresmia27@gmail.com>
2023-05-04 14:46:56 -0700

    Header styling phase 1

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,54 +1,46 @@
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Link, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-import heroImage from "../../assets/images/jb_hero.jpeg";
+import HomePageIcon from "../../components/icons/homepage_icon";
 import { colors } from "../../constants/colors";
 import customStyles from "../../styles/customStyles.module.css";
+import PersonalVideoIcon from "@mui/icons-material/PersonalVideo";
+
+const buttonStyle = {
+  backgroundColor: colors.primaryBlue,
+  borderRadius: "30px",
+};
 
 const Home: React.FunctionComponent = () => {
   return (
-    <Box>
-      <Grid
-        className={`${customStyles.heroImage}`}
-        display={"flex"}
-        flexDirection={"column"}
-        justifyContent={"center"}
-        height={"600px"}
-      >
-        <Typography
-          textAlign={"center"}
-          variant="h1"
-          color={colors.primaryWhite}
-        >
-          JB Associates
-        </Typography>
-        <Typography
-          textAlign={"center"}
-          variant="h4"
-          color={colors.primaryWhite}
-        >
-          Let us help you
-        </Typography>
-
-        <Box textAlign={"center"} marginTop={"15px"}>
-          <Button
-            variant="contained"
-            style={{ backgroundColor: colors.primaryBlue }}
-          >
-            Get started
-          </Button>
-          <Box textAlign={"center"} width={"40%"} margin={"20px auto"}>
-            <Typography
-              textAlign={"center"}
-              variant="h4"
-              color={colors.primaryWhite}
-            >
-              Executive Search & Recruitment Wealth Management, Legal and
-              Financial Services
-            </Typography>
+    <Box display={"flex"}>
+      <Box display={"flex"} width={"50%"}>
+        <Box width={"80%"} margin={"auto"}>
+          {" "}
+          <Typography variant="h2">
+            Bridging the gap between talent and opportunity and finance
+          </Typography>
+          <Box marginTop={"25px"}>
+            <Button style={buttonStyle} variant="contained">
+              Schedule a meeting!
+            </Button>
+          </Box>
+          <Box marginTop={"20px"} display={"flex"}>
+            <Link>Schedule a meeting!</Link>
+            <Box marginTop={"-3px"} marginLeft={"6px"}>
+              <PersonalVideoIcon />
+            </Box>
           </Box>
         </Box>
-      </Grid>
+      </Box>
+      <Box
+        width={"50%"}
+        display={"flex"}
+        justifyContent={"center"}
+        paddingTop={"130px"}
+      >
+        <HomePageIcon />
+      </Box>
     </Box>
   );
 };
