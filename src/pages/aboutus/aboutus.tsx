697cb3d39d7119204a3c2c39f8d1f931c607b921
IvanTorresMia <ivantorresmia27@gmail.com>
2023-05-04 17:29:28 -0700

    about us phase 1

--- src/pages/aboutus/aboutus.tsx
+++ src/pages/aboutus/aboutus.tsx
@@ -1,14 +1,79 @@
-import { Typography } from "@mui/material";
+import { Divider, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import daniel from "../../assets/images/daniel.png";
+import byron from "../../assets/images/byron.png";
 
 const AboutUs: React.FunctionComponent = () => {
   return (
-    <Box>
-      <Typography variant="subtitle2">About us</Typography>
+    <Box paddingTop={"40px"}>
+      <Typography marginBottom={"50px"} variant="h2" textAlign={"center"}>
+        Meet the team!
+      </Typography>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        width={"80%"}
+        margin={"auto"}
+      >
+        <Box
+          display={"flex"}
+          justifyContent={"space-evenly"}
+          marginBottom={"50px"}
+        >
+          <Box width={"50%"} paddingLeft={"100px"}>
+            <Typography variant="h5">Daniel Vargas</Typography>
+            <Divider />
+            <Typography
+              marginBottom={"20px"}
+              marginTop={"20px"}
+              variant="subtitle1"
+            >
+              I am Daniel, I crush at drums and do other stuff
+            </Typography>
+            <Typography variant="subtitle1">
+              {" "}
+              It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </Typography>
+          </Box>
+
+          <Box width={"50%"}>
+            <img style={{ width: "100%" }} src={daniel} alt="daniel vargas" />
+          </Box>
+        </Box>
+        <Box display={"flex"} justifyContent={"space-evenly"}>
+          <Box width={"50%"}>
+            <img style={{ width: "100%" }} src={byron} alt="daniel vargas" />
+          </Box>
+          <Box width={"50%"} paddingRight={"100px"}>
+            <Typography variant="h5">Byron Vargas</Typography>
+            <Divider />
+            <Typography
+              marginBottom={"20px"}
+              marginTop={"20px"}
+              variant="subtitle1"
+            >
+              I am Byron, CEO and unfortunately a Barcelona fan
+            </Typography>
+            <Typography variant="subtitle1">
+              {" "}
+              It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
-
 
 export default AboutUs;
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,12 +2,14 @@ import { Grid } from "@mui/material";
 import React from "react";
 import Home from "../home/home";
 import InfoCards from "../infocards/InfoCards";
+import AboutUs from "../aboutus/aboutus";
 
 const Main: React.FunctionComponent = () => {
   return (
     <Grid>
       <Home />
       <InfoCards />
+      <AboutUs />
     </Grid>
   );
 };
