08097d57375934887be9abe88725130d26b1028c
IvanTorresMia <ivantorresmia27@gmail.com>
2023-06-15 15:50:11 -0700

    fixing colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,9 +1,8 @@
 export const colors = {
-  primaryBlue: "#6667AB",
+  primary: "#6667AB",
   secondaryBlue: "#BODDE4",
   lightBlue: "#E2FOF9",
   primaryWhite: "#FFFFFF",
   primaryRed: "#DF4C73",
-  primaryBlack: "#000000",
   red: "#FE0000",
 };
