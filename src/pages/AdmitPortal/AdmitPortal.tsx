017ff309f622cbc0e1bd710cdf260c91e0fe0319
IvanTorresMia <ivantorresmia27@gmail.com>
2023-06-15 15:50:29 -0700

    jobs page

--- src/pages/AdmitPortal/AdmitPortal.tsx
+++ src/pages/AdmitPortal/AdmitPortal.tsx
@@ -1,10 +1,77 @@
-import { Grid } from "@mui/material";
-import React from "react";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
+import { Box, Button, Divider, Grid, Typography } from "@mui/material";
+import { useAuth } from "../../providers/authProvider";
+import React, { useCallback, useEffect, useState } from "react";
+import { app } from "../../index";
+import { theme } from "../../theme";
+
+// TODO: create admin portal
+type Jobs = {
+  id: string;
+  title: string;
+  description: string;
+  longDescription: string;
+};
 
 const AdminPortal: React.FunctionComponent = () => {
+  const [jobsAvailable, setJobsAvailable] = useState<Jobs[] | []>([]);
+  const user = useAuth();
+
+  const fetchJobs = useCallback(() => {
+    const db = getFirestore(app);
+
+    onSnapshot(query(collection(db, "jobs")), (snapshot) => {
+      const list: Jobs[] = [];
+      for (const job of snapshot.docs) {
+        const data = { ...(job.data() as any), id: job.id };
+        console.log(data);
+        list.push(data);
+      }
+      console.log(list);
+      console.log("hello");
+
+      setJobsAvailable(list);
+    });
+  }, []);
+
+  useEffect(() => {
+    return () => fetchJobs();
+  }, [fetchJobs]);
+
   return (
     <Grid container>
-      <Grid item></Grid>
+      <Grid item xs={12} paddingTop={theme.spacing(12)}>
+        <Typography textAlign={"center"} variant="h1">
+          Welcome admin
+        </Typography>
+      </Grid>
+
+      {/* TODO: create form to add */}
+
+      {jobsAvailable.map((job, i) => (
+        <Grid item xs={12}>
+          <Box
+            padding={theme.spacing(4)}
+            display={"flex"}
+            alignItems={"center"}
+          >
+            <Typography variant="subtitle1" paddingRight={theme.spacing(20)}>
+              {job.title}
+            </Typography>
+            <Typography variant="subtitle2" paddingRight={theme.spacing(20)}>
+              {job.description}
+            </Typography>
+            <Button variant="outlined">Delete</Button>
+          </Box>
+          <Divider />
+        </Grid>
+      ))}
     </Grid>
   );
 };
