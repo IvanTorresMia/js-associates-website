df55e832e0c128a4cc90835e8c315e55ffe47c5d
IvanTorresMia <ivantorresmia27@gmail.com>
2023-06-15 15:49:26 -0700

    adding config for firebase

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,24 @@ import App from "./App";
 import { getApps } from "firebase/app";
 import reportWebVitals from "./reportWebVitals";
 import { initializeApp } from "firebase/app";
-import { config } from "../config/config";
+// import { config } from "./config/config";
 import { AuthProvider } from "./providers/authProvider";
+import { fetchEnvVars } from "./api/vars";
+import { config } from "./config/config";
+
+// let config = {};
+
+// const getConfigVars = async () => {
+//   try {
+//     const res = await fetchEnvVars();
+//     config = res;
+//     console.log(res);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// getConfigVars();
 
 export const app =
   getApps().length === 0 ? initializeApp(config) : getApps()[0];
